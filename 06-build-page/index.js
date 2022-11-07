768faa6579daddee7f3ffdcee6a21bbca93d5339
AnnaFeona <glavatskikh.anna@gmail.com>
2022-11-08 01:06:37 +0300

    feat: add task 6 solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
 const { copyDir } = require('../04-copy-directory/index');
-const { mergeCSS } = require('../05-merge-styles/index')
+const { mergeCSS } = require('../05-merge-styles/index');
 
 const newFolder = path.join(__dirname, 'project-dist');
 const assetsFolder = path.join(__dirname, 'assets');
@@ -12,23 +12,31 @@ const templateFile = path.join(__dirname, 'template.html');
 buildHTML();
 
 async function buildHTML() {
-  copyDir(assetsFolder, path.join(newFolder, 'assets'));
-  mergeCSS(styleFolder, newFolder, 'style.css');
+  await copyDir(assetsFolder, path.join(newFolder, 'assets'));
+  await mergeCSS(styleFolder, newFolder, 'style.css');
 
   const components = await fs.readdir(componentsFolder, {withFileTypes: true});
   const componentsHtml = components.filter(item => path.extname(item.name) === '.html');
   const componentsNames = componentsHtml.map(item => item.name);
   const componentsKeys = componentsNames.map(item => item.replace('.html', ''));
-  console.log(componentsNames, componentsKeys);
 
+  const componentsContent = await Promise.all(componentsKeys.map(item => {
+    return fs.readFile(path.join(componentsFolder, item + '.html'), 'utf-8');
+  }))
 
-  const templateContent = await fs.readFile(templateFile);
+  const content = componentsKeys.map((key, index) => {
+    let obj = {};
+    obj[key] = componentsContent[index];
+    return obj;
+  })
+  const templateContent = await fs.readFile(templateFile, 'utf-8');
+  let indexHtml = templateContent;
 
-  await fs.writeFile(path.join(newFolder, 'index.html'), templateContent);
+  content.forEach(item => {
+    for (let key in item) {
+      indexHtml = indexHtml.replace(`{{${key}}}`, item[key])
+    }
+  })
 
-  // console.log(templateContent.toString());
+  await fs.writeFile(path.join(newFolder, 'index.html'), indexHtml);
 }
-
-
-
-
